265c69521e705beb2375ca8ec954144300da29a7
waszqa04 <pwaszkiewicz2004@gmail.com>
2025-01-25 21:53:28 +0100

    Update zadanie_projektowe.cpp
    1 Funkcja

--- zadanie_projektowe.cpp
+++ zadanie_projektowe.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
-
+#include <algorithm>
 
 using namespace std;
 
+int obliczOdleglosc(const int odleglosci[], int n, int start, int koniec, int calkowitaOdleglosc) {
+    int odlegloscZgodnieZRuchem = 0;
+
+    for (int i = start; i != koniec; i = (i + 1) % n) {
+        odlegloscZgodnieZRuchem += odleglosci[i];
+    }
+
+    int odlegloscPrzeciwnieDoRuchu = calkowitaOdleglosc - odlegloscZgodnieZRuchem;
+
+    return min(odlegloscZgodnieZRuchem, odlegloscPrzeciwnieDoRuchu);
+}
 
 int main()
 {
