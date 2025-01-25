48dfc4ec5074bfd921d69060bbaa89b21cfc850a
waszqa04 <pwaszkiewicz2004@gmail.com>
2025-01-26 00:13:21 +0100

    Update zadanie_projektowe.cpp

--- zadanie_projektowe.cpp
+++ zadanie_projektowe.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <algorithm>
+#include <fstream>
 
 using namespace std;
+
+const int MAX_N = 100;
 
 int obliczOdleglosc(const int odleglosci[], int n, int start, int koniec, int calkowitaOdleglosc) {
     int odlegloscZgodnieZRuchem = 0;
@@ -24,14 +27,36 @@ void znajdzNajdalszePary(const int odleglosci[], int n, int najdalszePary[][2], 
     
     int maksymalnaOdleglosc = 0;
     liczbaPar = 0;
-	
-	
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int dystans = obliczDystans(dystanse, n, i, j, calkowityDystans);
+
+            if (dystans > maksymalnyDystans) {
+                maksymalnyDystans = dystans;
+                liczbaPar = 0;
+                najdalszePary[liczbaPar][0] = i + 1; 
+                najdalszePary[liczbaPar][1] = j + 1;
+                liczbaPar++;
+            } else if (dystans == maksymalnyDystans) {
+                najdalszePary[liczbaPar][0] = i + 1;
+                najdalszePary[liczbaPar][1] = j + 1;
+                liczbaPar++;
+            }
+        }
+    }
 }
 
 
 int main()
 {
+ifstream plikWejsciowy("wejscie.txt");
+ofstream plikWyjsciowy("wyniki.txt");
 
+
+
+plikWejsciowy.close();
+plikWyjsciowy.close();
 
 return 0;
 }
